16925edc3cebecbe5327cf4ae984d58df0c1460e
DesireeTirabosque <desireetirabosque@gmail.com>
2022-05-31 23:07:46 -0300

    Subimos Ejercicio Clase 5

---
+++ EjercicioClase5/EjercicioClase5/Program.cs
@@ -0,0 +1,21 @@
+﻿var respuesta = "S";
+
+while (respuesta.ToUpper() == "S")
+
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre:");
+    string nombre = Console.ReadLine();
+    Console.WriteLine($"¡Hola : {nombre}!");
+    Console.WriteLine("Desea continuar ? (S/N)");
+    respuesta = Console.ReadLine();
+}
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine(" Opción no válida");
+}
+
